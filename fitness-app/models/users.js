1e87f87866476e719647a82f1d27fa4a14210de6
hayden <haydenburger43@gmail.com>
2022-10-04 20:05:54 -0500

    added contructor for mongo

---
+++ fitness-app/models/users.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    userName: {
+      type: String
+    },
+
+    email: {
+     type: String
+    },
+
+    password: {
+      type: String,
+    },
+  });
