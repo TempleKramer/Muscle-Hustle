cbd2f0b1005e222bf24e5e273e6ccd0b9fff5118
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-10-08 09:15:21 -0500

    added client and server side files

--- fitness-app/config/connection.js
+++ fitness-app/config/connection.js
@@ -1,11 +1,9 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/fitness-app",
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
-
 module.exports = mongoose.connection;
