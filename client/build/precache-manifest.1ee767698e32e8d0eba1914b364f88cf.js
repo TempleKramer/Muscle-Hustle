11a6bac6efe8621caee526d6258145004931d4ff
Jarmaris Chaney <jarmaris.chaney@yahoo.com>
2022-10-14 14:46:06 -0500

    updated dependencies

---
+++ client/build/precache-manifest.1ee767698e32e8d0eba1914b364f88cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5249506e7da8775c0a5b5e88b8f3101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf29ee96e53161af111",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "0afb9df2415c912b0957",
+    "url": "/static/css/main.e119f5cf.chunk.css"
+  },
+  {
+    "revision": "aaf29ee96e53161af111",
+    "url": "/static/js/2.5b072de4.chunk.js"
+  },
+  {
+    "revision": "27d11278e7f20b13806cbf285cf73092",
+    "url": "/static/js/2.5b072de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f8a6d8bb37a4aaeeb7",
+    "url": "/static/js/3.c5d1ec51.chunk.js"
+  },
+  {
+    "revision": "0afb9df2415c912b0957",
+    "url": "/static/js/main.46d85b6d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.46d85b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288892a57cf11a486236",
+    "url": "/static/js/runtime-main.1a1f2779.js"
+  }
+]);
